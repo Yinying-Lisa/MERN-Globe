1cc76f766fd97f49d82b9c2a6638d9851256d240
Yinying-Lisa <yyli@seas.upenn.edu>
2021-09-11 13:10:46 -0400

    fixed dependency issues and added button

--- src/Button.js
+++ src/Button.js
@@ -1,43 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import './Button.css';
-
-const Button = props => {
-  if (props.href) {
-    return (
-      <a
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
-        href={props.href}
-      >
-        {props.children}
-      </a>
-    );
-  }
-  if (props.to) {
-    return (
-      <Link
-        to={props.to}
-        exact={props.exact}
-        className={`button button--${props.size || 'default'} ${props.inverse &&
-          'button--inverse'} ${props.danger && 'button--danger'}`}
-      >
-        {props.children}
-      </Link>
-    );
-  }
+export default function Button({ label, onClick }) {
   return (
-    <button
-      className={`button button--${props.size || 'default'} ${props.inverse &&
-        'button--inverse'} ${props.danger && 'button--danger'}`}
-      type={props.type}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
+    <button className="button" onClick={onClick}>
+      {label}
     </button>
   );
-};
-
-export default Button;
+}
